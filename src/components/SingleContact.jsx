7cb02711c188d217e0c92f6af08252dd634dfe78
AlleriaDev <142839879+AlleriaDev@users.noreply.github.com>
2025-03-30 23:01:36 +0200

    Updated HTML and components

--- src/components/SingleContact.jsx
+++ src/components/SingleContact.jsx
@@ -2,12 +2,22 @@ import React from "react";
 
 const SingleContact = ({ contact }) => {
     return (
-        <div className="card">
-                <div className="card-body">
-                    <h5 className="card-title">{contact.name} {contact.last_name}</h5>
-                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
+        <div className="card mt-2">
+            <div className="card-body">
+                <div className="row d-flex">
+                    <div className="col-2"><img className="img-thumbnail rounded float-start" src={contact.avatar}></img></div>
+                    <div className="col-8">
+                        <h5 className="card-title">{contact.name} {contact.last_name}</h5>
+                        <div className="card-text"><i class="fa-solid fa-phone"></i> {contact.phone}</div>
+                        <div className="card-text"><i class="fa-solid fa-envelope"></i> {contact.email}</div>
+                        <div className="card-text"><i class="fa-solid fa-location-dot"></i> {contact.address}</div>
+                    </div>
+                    <div className="col-2 d-flex flex-column">                            
+                        <button className="btn btn-secondary"><i class="fa-solid fa-trash"></i></button>
+                        <button className="btn btn-primary mt-2"><i class="fa-solid fa-pen-to-square"></i></button>
+                    </div>
                 </div>
+            </div>
         </div>
     );
 }
--- src/pages/ContactList.jsx
+++ src/pages/ContactList.jsx
@@ -1,20 +1,21 @@
 import {React, useState} from "react";
 
+import rigoImageUrl from "../assets/img/rigo-baby.jpg";
+
 import SingleContact from "../components/SingleContact"
 
 export const ContactList = () => {
     const [contacts, setContacts] = useState([
-        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21" },
-        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21" },
-        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21" },
-        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21" },
-        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21" },
+        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21", avatar:rigoImageUrl },
+        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21", avatar:rigoImageUrl },
+        { name: "Sergio", last_name: "Zarraga", phone: "600 600 600", email: "a@a.com", address: "Calle Petunia 21", avatar:rigoImageUrl },
     ]);
 
 
     return (
-        <div className="container">
-            <h1>Contact List</h1>
+        <div className="container contactList">
+            <div className="text-center"><h1>Contact List</h1></div>
+            <div className="d-flex justify-content-end"><button className="btn btn-primary addContact">Add Contact</button></div>
             <ul className="list-group mt-3">
                 {contacts.map((contact, index) => (
                    <SingleContact contact={contact} key={index} />
